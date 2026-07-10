7dbf0da8b397b90b8c1b1e6458c6231b9855efe4
Oluyale Ezekiel <amusecode@gmail.com>
2026-07-10 16:34:47 +0100

    fix: GROQ not GORQ

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    GORQ_API_KEY: str = os.getenv("GORQ_API_KEY")
+    GROQ_API_KEY: str = os.getenv("GROQ_API_KEY")
     GROQ_MODEL='llama-3.3-70b-versatile'
     GROQ_FALLBACK_API_KEY: str = os.getenv("GROQ_FALLBACK_API_KEY")
 
