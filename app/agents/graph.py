cfb7d13b71dc5e45043f6d8e0d65cb4ea2bbf2d7
Oluyale Ezekiel <amusecode@gmail.com>
2026-07-20 23:14:26 +0100

    refactor: clean the graph and memory logic

--- app/agents/graph.py
+++ app/agents/graph.py
@@ -1,14 +1,11 @@
 from langgraph.graph import StateGraph, END
-from langgraph.checkpoint.memory import MemorySaver
 from app.agents.state import AgentState
 from app.agents.nodes.planner import planner_node
 from app.agents.nodes.retriever import retrieve_node
 from app.agents.nodes.responder import generate_node
 
-
 # 1. Initialize the State Graph
 workflow = StateGraph(AgentState)
-
 
 # 2. Define the Nodes
 workflow.add_node("planner", planner_node)
@@ -40,12 +37,3 @@ workflow.add_conditional_edges(
 
 workflow.add_edge("retriever", "responder")
 workflow.add_edge("responder", END)
-
-
-# --- MEMORY UPGRADE ---
-# MemorySaver allows the agent to remember conversations based on 'thread_id'
-checkpointer = MemorySaver()
-
-
-# 4. Compile the Graph with Memory
-rag_agent = workflow.compile(checkpointer=checkpointer)
